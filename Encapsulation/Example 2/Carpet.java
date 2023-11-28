44355aa8ba74978aa76058cab109c144706a861b
smsatheesh <smsatheesh2000@gmail.com>
2023-11-28 20:17:17 +0530

    Encapsulation extra program added to the community

---
+++ Encapsulation/Example 2/Carpet.java
@@ -0,0 +1,15 @@
+class Carpet {
+	
+	double cost;
+	
+	public Carpet( double cost ) {
+		if( cost < 0 )
+			this.cost = 0;
+		else
+			this.cost = cost;
+	}
+	
+	public double getCost() {
+		return this.cost;
+	}
+}
