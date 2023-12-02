4e51f9278d37d201cce02cdf3b68025b57cf995e
smsatheesh <147731426+smsatheesh@users.noreply.github.com>
2023-12-02 12:37:41 +0530

    Update Main.java

--- Ineritance/Hierarchical-Level-Inheritance/Main.java
+++ Ineritance/Hierarchical-Level-Inheritance/Main.java
@@ -2,10 +2,9 @@ public class Main {
 	
 	public static void main(String[] args) throws Exception {
 
-		Programmer childObjForProgrammer = new Programmer();
-		
 		System.out.println( "\t\tHierarchical-Level Inheritance in  Java" );
 		
+		Programmer childObjForProgrammer = new Programmer();
 		System.out.println( "Basic salary of an employee           : " + childObjForProgrammer.salary );
 		System.out.println( "\nBonus for the Programmer              : " + childObjForProgrammer.programmerAddOnSalary );
 		System.out.println( "Total salary for a role of Programmer : " + ( childObjForProgrammer.salary + childObjForProgrammer.programmerAddOnSalary ) );
