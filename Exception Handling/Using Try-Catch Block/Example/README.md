18b1a6a93c872f2648e32aba3093fc5fef28b9a0
smsatheesh <smsatheesh2000@gmail.com>
2023-12-17 22:31:35 +0530

    Exception handling added to the java community with example program

---
+++ Exception Handling/Using Try-Catch Block/Example/README.md
@@ -0,0 +1,9 @@
+# Exception Handling `( JAVA Version )`
+
+## Try-Catch Blocks
+
+>>> Here, we have handled exception using try catch block.
+>>> Get 2 number from the user. If second number or divisor is said to be zero then exception event occurs which have been handled
+>>> This is an example `usages of try-catch block`.
+
+### Contributed by [Sm Satheesh](https://github.com/smsatheesh)
---
+++ Exception Handling/Using Try-Catch Block/Example/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+	static int getInt() {
+		Scanner __scanner = new Scanner( System.in );
+		return __scanner.nextInt();
+	}
+	
+	public static void main( String[] args ) {
+		
+		System.out.println( "\t\tException Handling" );
+		try {
+			
+			System.out.print( "Enter num1 : " );
+			int num1 = getInt();
+			
+			System.out.print( "Enter num2 : " );
+			int num2 = getInt();
+			System.out.println( "No exception event occurs. \nFinal output is : " + (num1 / num2) );
+		} catch ( Exception exp ) {
+			
+			exp.printStackTrace();
+		}
+		
+	}
+}
