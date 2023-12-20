501b95e7c43d8507988b69459e29140c72b25296
smsatheesh <smsatheesh2000@gmail.com>
2023-12-20 20:45:29 +0530

    regex example program in java added to the java community

---
+++ Regex/Example1/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main (String[] args) {
+    
+    	System.out.println( Pattern.matches( "[Ss]" , "Subject") );
+    	System.out.println( Pattern.matches( "[Subject]", "s" ) );
+    	System.out.println( Pattern.matches( "[^Satheesh]" , "z" ) );
+    	System.out.println( Pattern.matches( "[a-zA-z]" , "a" ) );
+    	System.out.println( Pattern.matches( "[a-z][A-z]" , "aA" ) );
+    	System.out.println( Pattern.matches( "[a-dx-z]" , "r" ) );
+    	System.out.println( Pattern.matches( "\\d", "ws" ) );
+    	System.out.println( Pattern.matches( "\\s" , " " ) );
+    	System.out.println( Pattern.matches( "\\w", "a" ) );
+    	System.out.println( Pattern.matches( "\\W", "+" ) );
+    	System.out.println( Pattern.matches( "[a-z && [^a-d]]" , "t"  ) );
+    }
+}
