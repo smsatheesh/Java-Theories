8d78357d93b78ea55b8f2bcb5704533631a5b8fa
smsatheesh <smsatheesh2000@gmail.com>
2023-12-25 10:05:15 +0530

    folders re-arranged and added multi-threading with example to the JAVA community.

---
+++ Threads/Multi-Threading/Example1/README.md
@@ -0,0 +1,6 @@
+# MULTI-THREADING `( JAVA Version )`
+
+>>> Multithreading in Java, is a process of executing multiple threads simultaneously.
+>>> Threads share a common memory area well known as heap, so that multiple threads can be worked together.
+
+### Contributed by [Sm Satheesh](https://github.com/smsatheesh)
---
+++ Threads/Multi-Threading/Example1/src/Add.java
@@ -0,0 +1,28 @@
+class Add {
+	
+	int i;
+	public void doCountDown() {
+		
+		String color;
+		
+		switch( Thread.currentThread().getName() ) {
+		
+			case "Thread 1":
+				color = ThreadColor.red;
+				break;
+			case "Thread 2":
+				color = ThreadColor.green;
+				break;
+			default:
+				color = ThreadColor.blue;
+				break;
+		}
+		
+		int n = 0;
+		for( int i = 0; i < 10; i++ ) {
+			
+			n++;
+			System.out.println( color + " " + Thread.currentThread().getName() + " : n = " + n );
+		}
+	}
+}
